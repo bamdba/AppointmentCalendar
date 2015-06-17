d979c66fdc07d5c4a55f6860ca5a6af611de0355
Marius Zamfir <cubuso@outlook.com>
2015-06-17 16:10:01 +0100

    ASP.NET - MVC4 Razor
    Dependencies -  Entity Framework and Bootstrap
    Basic views: day, week and month
    Convenient drag-and-drop
    Highlighted current day
    Dynamic data loading and saving

---
+++ AppointmentCalendar/TaskManager.MVC5/Models/TaskDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TaskManager.MVC5.Model;
+
+namespace TaskManager.MVC5.Models
+{
+    public class TaskDetails
+    {
+        public Task Task { get; set; }
+        public IEnumerable<Status> Statuses { get; set; }
+        public TaskDetails(Task ts, IEnumerable<Status> st)
+        {
+            Task = ts;
+            Statuses = st;
+        }
+    }
+}
